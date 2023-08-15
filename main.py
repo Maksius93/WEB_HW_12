50cef2e369052d52ade49b5d9ab6885bdd3fc396
Maksius93 <m.g.fursa@gmail.com>
2023-08-15 17:29:27 +0300

    HW_12

---
+++ main.py
@@ -0,0 +1,26 @@
+import uvicorn
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from src.routes import contacts, auth
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth.router)
+app.include_router(contacts.router, prefix='/api')
+
+
+@app.get("/")
+def read_root():
+    return {"message": "CONTACT API"}
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="localhost", reload=True, log_level="info", port=5000)
